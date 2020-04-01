ac4bd4debb00ba5c9c5986ec2eb286a5182b6bf4
phunkyfish <phunkyfish@gmail.com>
2020-04-01 19:41:44 +0100

    SPDX, headers and badges

--- src/stream/threads/Thread.cpp
+++ src/stream/threads/Thread.cpp
@@ -1,11 +1,9 @@
 /*
- *  Copyright (c) 2002 Frodo
- *      Portions Copyright (c) by the authors of ffmpeg and xvid
- *  Copyright (C) 2002-2018 Team Kodi
- *  This file is part of Kodi - https://kodi.tv
+ *  Copyright (C) 2005-2020 Team Kodi
+ *  https://kodi.tv
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
- *  See LICENSES/README.md for more information.
+ *  See LICENSE.md for more information.
  */
 
 #define __STDC_FORMAT_MACROS
