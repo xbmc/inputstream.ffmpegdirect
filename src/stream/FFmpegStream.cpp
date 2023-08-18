b330f6c92d80a7af931807f6ce39e1ed6a3e0c9d
matthuisman <gitlab@matthuisman.nz>
2023-08-19 08:38:35 +1200

    test

--- src/stream/FFmpegStream.cpp
+++ src/stream/FFmpegStream.cpp
@@ -183,7 +183,7 @@ bool FFmpegStream::Open(const std::string& streamUrl, const std::string& mimeTyp
     std::string test = CURL::GetRedacted(streamUrl);
     Log(LOGLEVEL_DEBUG, "redacted ok");
     Log(LOGLEVEL_DEBUG, "redacted: %s", test);
-    std::string test2 = "HelloWorld";
+    std::string test2 = "HelloWorld%20N";
     av_dump_format(m_pFormatContext, 0, test2.c_str(), 0);
     av_dump_format(m_pFormatContext, 0, test.c_str(), 0);
   }
