7375d5a2aef69017116c42e93d314a0514ceb389
matthuisman <gitlab@matthuisman.nz>
2023-08-18 22:45:39 +1200

    remove duplicate and comment out

--- src/stream/FFmpegStream.cpp
+++ src/stream/FFmpegStream.cpp
@@ -179,7 +179,7 @@ bool FFmpegStream::Open(const std::string& streamUrl, const std::string& mimeTyp
   if (m_opened)
   {
     FFmpegLog::SetEnabled(true);
-    av_dump_format(m_pFormatContext, 0, CURL::GetRedacted(streamUrl).c_str(), 0);
+   // av_dump_format(m_pFormatContext, 0, CURL::GetRedacted(streamUrl).c_str(), 0);
   }
   FFmpegLog::SetEnabled(kodi::addon::GetSettingBoolean("allowFFmpegLogging"));
 
@@ -762,9 +762,6 @@ bool FFmpegStream::Open(bool fileinfo)
       }
     }
     Log(LOGLEVEL_DEBUG, "%s - av_find_stream_info finished", __FUNCTION__);
-
-    // print some extra information
-    //av_dump_format(m_pFormatContext, 0, CURL::GetRedacted(strFile).c_str(), 0);
 
     if (m_checkTransportStream)
     {
